d984ace16b7689df30d45d2b5db3c204c69ef10f
ponnanna123 <21cs02003@iitbbs.ac.in>
2024-04-19 22:47:18 +0530

    connect to mongodb

--- api/server.js
+++ api/server.js
@@ -1,7 +1,18 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => console.log(error));
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+app.listen(process.env.PORT_NO, () => {
+  console.log(`Server listening on port ${process.env.PORT_NO}`);
 });
